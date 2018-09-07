c00fed1138b22b077ff0d2ba71c6f7d570157c59
uno <st9007a@gmail.com>
2018-09-07 22:39:30 +0800

    Beautify web

--- web/app/app.js
+++ web/app/app.js
@@ -1,6 +1,7 @@
 import './index.pug'
 
 import 'semantic-ui-offline/semantic.css'
+import './app.sass'
 
 import * as tf from '@tensorflow/tfjs'
 import { loadFrozenModel } from '@tensorflow/tfjs-converter'
@@ -11,63 +12,53 @@ require('semantic-ui-offline/semantic.js')
 const MODEL_URL = './model/tensorflowjs_model.pb'
 const WEIGHTS_URL = './model/weights_manifest.json'
 
+const video = document.getElementsByTagName('video')[0]
+const canvas = document.getElementsByTagName('canvas')[0]
+
+const width = 640
+const height = 480
+
+let isCapture = false
+
+video.setAttribute('width', width)
+video.setAttribute('height', height)
+canvas.setAttribute('width', width)
+canvas.setAttribute('height', height)
+
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia
 
 loadFrozenModel(MODEL_URL, WEIGHTS_URL).then(main)
 
-let ids = []
 
 function main(model) {
-  console.log('Get model!')
+  const successCallback = (stream) => {
+    video.srcObject = stream
+  }
+
+  const errorCallback = (error) => {
+    console.log("navigator.getUserMedia error: ", error)
+  }
+
   navigator.getUserMedia({audio: false, video: true}, successCallback, errorCallback)
 
-  function successCallback(stream) {
-    window.stream = stream
-    document.getElementsByTagName('h1')[0].textContent = 'success'
+  $('.ui.button').click(e => {
 
-    const video = document.getElementsByTagName('video')[0]
-    const canvas = document.getElementsByTagName('canvas')[0]
+    $(e.currentTarget).toggleClass('teal').toggleClass('red')
+    isCapture = !isCapture
 
-    let width = 640
-    let height = 480
+    $(e.currentTarget).text(isCapture ? 'Stop' : 'Capture Your Face ID')
 
-    video.srcObject = stream
 
-    video.setAttribute('width', width)
-    video.setAttribute('height', height)
-    canvas.setAttribute('width', width)
-    canvas.setAttribute('height', height)
+    // const context = canvas.getContext('2d')
+    // context.drawImage(video, 0, 0, width, height)
+    //
+    // const data = context.getImageData(140, 220, 200, 200)
+    //
+    // const res = model.execute({
+    //   'squeeze_net/face_input': tf.fromPixels(data).reshape([1, 200, 200, 3]).asType('float32')
+    // })
+    //
+    // res.dataSync()
+  })
 
-    $('button').click(() => {
-      console.log(canvas)
-      const context = canvas.getContext('2d')
-
-      context.drawImage(video, 0, 0, width, height)
-      const data = context.getImageData(140, 220, 200, 200)
-
-      console.log(model)
-      const res = model.execute({
-        'squeeze_net/face_input': tf.fromPixels(data).reshape([1, 200, 200, 3]).asType('float32')
-      })
-      // res.print()
-      // console.log(res.dataSync()[0])
-      ids.push(res.dataSync())
-
-      if (ids.length > 1) {
-        for (let i = 1; i < ids.length; i++) {
-          let sum = 0
-          for (let j = 0; j < 128; j++) {
-            sum += Math.pow(ids[i][j] - ids[i - 1][j], 2)
-          }
-          console.log(sum)
-        }
-      }
-    })
-
-  }
-
-  function errorCallback(error){
-    console.log("navigator.getUserMedia error: ", error)
-    document.getElementsByTagName('h1')[0].textContent = 'fail'
-  }
 }
